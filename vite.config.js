ed31e60477955dec9a4ba92c050d411db0d77ef8
Larimar4you <Zajanezlaja@gmail.com>
2026-06-15 19:35:24 +0200

    GO

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src',
   base: '/goit-advancedjs-hw-04/',
   define: {
     global: 'globalThis',
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 });
